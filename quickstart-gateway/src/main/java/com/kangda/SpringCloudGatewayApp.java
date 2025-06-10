bcc3b8d8105ad74e80fa91a9e17aecb869608d43
jerryfan <jerryfan@kdvtc.edu.cn>
2025-06-10 08:09:14 +0800

    spring cloud gateway

---
+++ quickstart-gateway/src/main/java/com/kangda/SpringCloudGatewayApp.java
@@ -0,0 +1,12 @@
+package com.kangda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public  class SpringCloudGatewayApp {
+    public  static  void  main(String[] args) {
+
+        SpringApplication.run(SpringCloudGatewayApp.class , args);
+    }
+}
